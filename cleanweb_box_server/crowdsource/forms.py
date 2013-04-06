b961b13223ecaaf351bc1519959a763be89f8303
Ellie <elishevabina@gmail.com>
2013-04-06 18:13:58 -0300

    Update forms.py
    beginning to write forms.py

--- cleanweb_box_server/crowdsource/forms.py
+++ cleanweb_box_server/crowdsource/forms.py
@@ -1,1 +1,8 @@
-# Create form classes here.
+from django. import forms
+
+class SubmissionForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100)
+    boxID = forms.IntegerField()
+    picture= forms.ImageField()
+    
