f39e346c5e5df3807a99197bd99eeb4b3bd99814
Ellie <elishevabina@gmail.com>
2013-04-06 18:20:54 -0300

    Update forms.py
    finished?

--- cleanweb_box_server/crowdsource/forms.py
+++ cleanweb_box_server/crowdsource/forms.py
@@ -1,8 +1,10 @@
 from django. import forms
 
-class SubmissionForm(forms.Form):
+class UploadForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(max_length=100)
-    boxID = forms.IntegerField()
-    picture= forms.ImageField()
-    
+    IDno = forms.PositiveIntegerField()
+    picture= forms.ImageField(required=False)
+    latitude = forms.DecimalField()
+    longitude = forms.DecimalField()
+        
