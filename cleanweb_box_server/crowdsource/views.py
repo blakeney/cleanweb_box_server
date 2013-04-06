5b2887d80c0c304b589972d079e2a08e0c212dc6
ermain <ermain@mit.edu>
2013-04-06 18:35:40 -0300

    Update views.py

--- cleanweb_box_server/crowdsource/views.py
+++ cleanweb_box_server/crowdsource/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from polls.models import Poll
+from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, get_object_or_404
+
+def display_form(request):
+  
+def submit(request, username):
+
+def display_submissions(request):
+
+def userpage(request, username):
