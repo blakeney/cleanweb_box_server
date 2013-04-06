2d239ec8731f9098fd420e91a6e96f519aad3b2d
blakeney <blakeney@alum.mit.edu>
2013-04-06 17:46:21 -0400

    Added URLs, basic templates, and simple home view.

---
+++ cleanweb_box_server/crowdsource/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'views.home', name='crowdsource_home'),
+    url(r'^upload/', 'views.upload', name='crowdsource_upload'),
+)
