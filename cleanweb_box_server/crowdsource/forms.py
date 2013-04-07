0e81bb4e101f1c3645c969a7a5b3c70297cffbb3
Ellie <elishevabina@gmail.com>
2013-04-06 22:21:10 -0300

    Update forms.py
    added max_digits and decimal_places to DecimalFields

--- cleanweb_box_server/crowdsource/forms.py
+++ cleanweb_box_server/crowdsource/forms.py
@@ -5,6 +5,6 @@ class UploadForm(forms.Form):
     password = forms.CharField(max_length=100)
     IDno = forms.PositiveIntegerField()
     picture= forms.ImageField(required=False)
-    latitude = forms.DecimalField()
-    longitude = forms.DecimalField()
+    latitude = forms.DecimalField(decimal_places=6, max_digits=10)
+    longitude = forms.DecimalField(decimal_places=6, max_digits=10)
         
