c71c439a4575d02010b631b2a037742979bf6cad
Ellie <elishevabina@gmail.com>
2013-04-06 22:00:02 -0300

    Update urls.py
    added thanks url

--- cleanweb_box_server/crowdsource/urls.py
+++ cleanweb_box_server/crowdsource/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'views.home', name='crowdsource_home'),
     url(r'^upload', 'views.upload', name='crowdsource_upload'),
+    url(r'^thanks$', 'views.thanks', name=crowdsource_thanks'),
 )
