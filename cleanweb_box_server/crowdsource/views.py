1b71c12539ef7bf0a0fa7e27fcccf4e695c05e74
ermain <ermain@mit.edu>
2013-04-06 22:38:41 -0300

    Update views.py
    MORE INDENTATION :(

--- cleanweb_box_server/crowdsource/views.py
+++ cleanweb_box_server/crowdsource/views.py
@@ -10,7 +10,7 @@ from crowdsource.forms import UploadForm
 def upload(request):
 	if request.method == 'POST': # If the form has been submitted...
 		data = request.POST
-        form = UploadForm(data) # A form bound to the POST data
+		form = UploadForm(data) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
         					# is_valid() is a function provided by django (don't need to overload)
             #data fields: username, password, IDno, picture, latitude, longitude
@@ -29,18 +29,19 @@ def upload(request):
         			latitude = form.cleaned_data['latitude'],
         			longitude = form.cleaned_data['longitude'])
             	return HttpResponseRedirect('/thanks/') # Redirect after POST
-    else: #display blank form
+	else: #display blank form
         form = UploadForm() # An unbound form
-    return render(request, 'upload_form.html', {
+    	return render(request, 'upload_form.html', {
         'form': form,
     })
     
 #display thanks page
 def thanks(request):
-        return HttpResponse("Thank you! Congratz!")
+	return HttpResponse("Thank you! Congratz!")
 
 #display submissions already uploaded (can be reached as a static webpage or as a redirect from upload page)
 def display_submissions(request):
-
+	pass
 #display data associated with a user
 def userpage(request, username):
+	pass
